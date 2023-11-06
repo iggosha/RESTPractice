5bd9b83bdb04b115647d179f2930c81726403e42
Igor <81161586+iggosha@users.noreply.github.com>
2023-11-06 06:00:57 +0300

    Delete src directory

--- src/main/java/com/myrest/restpractice/excs/InvalidRequestParametersException.java
+++
@@ -1,7 +0,0 @@
-package com.myrest.restpractice.excs;
-
-public class InvalidRequestParametersException extends GlobalException {
-    public InvalidRequestParametersException(String requestParameterName) {
-        super(400, "Invalid request parameter value = '" + requestParameterName + "'");
-    }
-}
--- src/main/java/com/myrest/restpractice/excs/MicrochipNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.myrest.restpractice.excs;
-
-public class MicrochipNotFoundException extends GlobalException {
-
-    public MicrochipNotFoundException(long id) {
-        super(404, "Microchip not found with id=" + id);
-    }
-}
